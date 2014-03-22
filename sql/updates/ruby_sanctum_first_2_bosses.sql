a4fb1af64576b35848bcc062e1489ec9d5aff30a
Mack36 <raofl@mail.ru>
2014-03-22 23:09:10 +0500

    RS added instance bind flag to first 2 bosses on 25h

---
+++ sql/updates/ruby_sanctum_first_2_bosses.sql
@@ -0,0 +1,3 @@
+--- Add Instance binding to first 2 bosses in RS heroic mods
+UPDATE `creature_template` SET `flags_extra`='1' WHERE (`entry`='39920')  
+UPDATE `creature_template` SET `flags_extra`='1' WHERE (`entry`='39823')  
