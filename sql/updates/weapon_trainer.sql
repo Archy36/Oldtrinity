185d7aac348824e7caab1847bc955fa7bb55d866
Mack36 <raofl@mail.ru>
2014-04-15 01:27:34 +0500

    Two side all in one weapon trainer

---
+++ sql/updates/weapon_trainer.sql
@@ -0,0 +1,18 @@
+INSERT INTO creature_template VALUES
+(190040, 0, 0, 0, 0, 0, 30414, 0, 0, 0, 'Master Shi-Fu', 'Weapon Master', NULL, 5271, 50, 50, 0, 35, 35, 81, 1, 1.14286, 4, 1, 85, 112, 0, 206, 2.4, 1500, 0, 1, 0, 2048, 8, 0, 2, 0, 0, 0, 58, 86, 20, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 7, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 2, '', 12340);
+
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 200, 10000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 201, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 202, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 227, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 1180, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 5011, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 199, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 198, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 15590, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 2567, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 264, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 196, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 197, 1000, 0, 0, 0);
+INSERT INTO npc_trainer (entry, spell, spellcost, reqskill, reqskillvalue, reqlevel) VALUES(190040, 266, 1000, 0, 0, 0);
+
