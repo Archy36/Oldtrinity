6cf57fbd599f3f89b1c869a52c36d8f58ce967ad
Mack36 <raofl@mail.ru>
2014-04-27 01:02:48 +0500

    Fix DK Rise Ghoul cd bug
    Upd queldelar HoR quest related creature.

--- sql/updates/keldelar sunwell quest.sql
+++ sql/updates/keldelar sunwell quest.sql
@@ -5,3 +5,4 @@ UPDATE `quest_template` SET `Flags`=192, `Type`=62 where `id`=24598;
 UPDATE `quest_template` SET `Flags`=192, `Type`=62 where `id`=24594;
 UPDATE `quest_template` SET `Flags`=192, `Type`=62 where `id`=24596;
 UPDATE `quest_template` SET `Flags`=65536 where `id`=12856;
+UPDATE `creature_template` SET `unit_flags` = 512 where `id`=37158;
