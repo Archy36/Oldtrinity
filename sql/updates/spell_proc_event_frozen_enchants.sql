12a3c9eeaf84f73e2a5d02d82efad1fc0f7c9cb4
Mack36 <raofl@mail.ru>
2014-06-20 21:46:37 +0500

    Fix some enchants and Midsummer event quests

---
+++ sql/updates/spell_proc_event_frozen_enchants.sql
@@ -0,0 +1,3 @@
+INSERT INTO `spell_proc_event` VALUES
+(46662, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45);
+UPDATE `spell_enchant_proc_data` SET `entry` = 1894,`customChance` = 0,`PPMChance` = 0.5,`procEx` = 0 WHERE `spell_enchant_proc_data`.`entry` = 1894;
---
+++ sql/updates/summerfest.sql
@@ -0,0 +1,9 @@
+UPDATE `quest_template` SET `Flags`='69634' WHERE (`Id`='11921');
+UPDATE `quest_template` SET `Flags`='69634' WHERE (`Id`='11924');
+UPDATE `quest_template` SET `Flags`='69634' WHERE (`Id`='11925');
+UPDATE `quest_template` SET `Flags`='69634' WHERE (`Id`='11926');
+UPDATE `quest_template` SET `Flags`='65538' WHERE (`Id`='11731');
+UPDATE `quest_template` SET `Flags`='65538' WHERE (`Id`='11922');
+UPDATE `quest_template` SET `Flags`='65538' WHERE (`Id`='11657');
+UPDATE `quest_template` SET `Flags`='65538' WHERE (`Id`='11923');
+
