24484cd75db540435edcc67cfb2b0c266e468bd1
Mack36 <raofl@mail.ru>
2014-06-20 22:28:41 +0500

    Shadow's Edge plus Shadowmourne bug

---
+++ sql/updates/shadows_edge_shm_bug.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `conditions` VALUES
+(15, 10910, 1, 0, 0, 2, 0, 49888, 1, 0, 1, 0, 0, '', 'Highlord Darion Mograine: Show Gossip option if player does not have Shadow''s Edge'),
+(15, 10910, 1, 0, 0, 8, 0, 24549, 0, 0, 1, 0, 0, '', 'Highlord Darion Mograine: Show Gossip option if player has quest 24549 not rewarded\r\n'),
+(15, 10910, 1, 0, 0, 8, 0, 24743, 0, 0, 0, 0, 0, '', 'Highlord Darion Mograine: Show Gossip option if player has quest 24743 rewarded'),
+(15, 10910, 1, 0, 0, 8, 0, 24912, 0, 0, 1, 0, 0, '', 'Highlord Darion Mograine: Hide Gossip option if player has quest 24912 rewarded');
